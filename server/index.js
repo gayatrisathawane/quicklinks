dca2d9e2f2f223ddc8add160b0150bb519615a0a
gayatrisathawane <gayatripsathawane@gmail.com>
2023-11-08 17:38:43 +0530

    feature post api created

--- server/index.js
+++ server/index.js
@@ -7,29 +7,58 @@ import Link from './models/link.js'
 
 const app = express();
 app.use(express.json())
-const PORT =process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 
 //connect mongodb
 
-const mongodb = async()=>{
-   try{
-    const connect = await mongoose.connect('mongodb+srv://gayatrisathawane:gayatri123@cluster0.cqdx7ze.mongodb.net/quicklinks')
-    if(connect){
-        console.log('connected')
+const mongodb = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI)
+    if (connect) {
+      console.log('connected')
     }
-   }
-   catch(err){
+  }
+  catch (err) {
     console.log(err)
-   }
+  }
 }
 mongodb();
 
+///post method
+app.post('/api/link', async(req, res) => {
+  const { url, slug } = req.body;
+  const randomSlug = Math.random().toString(36).substring(2, 7)
 
+  const link = new Link({
+    url: url,
+    slug: slug || randomSlug
 
+  })
+  try {
+    const saveLink = await link.save();
+    return res.json({
+      success: true,
+      data: {
 
+        shortUrl: `${process.env.BASE_URL}/${saveLink.slug}`
+      
+      },
+      message:"Link saved successfully"
+    }
+    )
 
+  } catch (error) {
+   return res.json({
+      success: false,
+      message: error.message
 
-app.listen(PORT , ()=>{
-    console.log(`port running on ${PORT}`)
+    })
+
+  }
+
+})
+
+app.listen(PORT, () => {
+  console.log(`port running on ${PORT}`)
 }
 )
